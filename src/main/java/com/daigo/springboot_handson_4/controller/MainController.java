2a176d7392294ee13400ea56f1fa1e86986bb333
olaiojo <akhr.daigo@gmail.com>
2020-10-28 21:11:47 +0900

    feat: start using localsearch API.

--- src/main/java/com/daigo/springboot_handson_4/controller/MainController.java
+++ src/main/java/com/daigo/springboot_handson_4/controller/MainController.java
@@ -9,8 +9,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Objects;
-
 @Controller
 public class MainController {
     private static final RestTemplate restTemplate = new RestTemplate();
@@ -18,12 +16,13 @@ public class MainController {
     @GetMapping("/index")
     public String index(Model model) {
         //開発用に湘南台駅の文字列を指定。
+        //TODO: ユーザ入力を受け取る機能
         final SampleLocation userLocation = new SampleLocation("湘南台駅");
-        model.addAttribute("userLocation", userLocation.getSAMPLE_LOCATION());
 
-        //TODO: userLocationをコンテンツジオコーダAPIに渡してレスポンスをバインド
-        //リクエスト先URL: コンテンツジオコーダAPI
-        final String REQUEST_URL = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder?appid=dj00aiZpPVd0eW04MVNNRGtUbSZzPWNvbnN1bWVyc2VjcmV0Jng9YTc-&query=湘南台駅&category=address&output=json";
+        //userLocationをコンテンツジオコーダAPIに渡してレスポンスをバインド
+        //リクエスト先: コンテンツジオコーダAPI
+        final String LOCATION = userLocation.getSAMPLE_LOCATION();
+        final String REQUEST_URL = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder?appid=dj00aiZpPVd0eW04MVNNRGtUbSZzPWNvbnN1bWVyc2VjcmV0Jng9YTc-&query=" + LOCATION + "&category=landmark&output=json";
         //ContentsGeoCoderクラスへのバインドをtry
         ContentsGeoCoder contentsGeoCoder = new ContentsGeoCoder();
         try {
@@ -35,16 +34,25 @@ public class MainController {
             System.out.println("|||||||||| Error 5XX ||||||||||");
             throw e;
         }
-        model.addAttribute("geo", contentsGeoCoder.getResultInfo().getDescription());
 
-        System.out.println("==========debug zone===========");
-        System.out.println(REQUEST_URL);
-        System.out.println("contentsGeoCoder nonNull? : " + Objects.nonNull(contentsGeoCoder)); //true
-        System.out.println("ResultInfo nonNull? : " + Objects.nonNull(contentsGeoCoder.getResultInfo())); // false
-        System.out.println("Feature nonNull? : " + Objects.nonNull(contentsGeoCoder.getFeatureList())); // false
-        System.out.println("Request Response" + restTemplate.getForObject(REQUEST_URL, String.class));
-        System.out.println(contentsGeoCoder.getFeatureList().get(0).getGeometry().getCoordinates());
-        System.out.println("==========/debug zone===========");
+        //Modelにadd
+        model.addAttribute("userLocation", contentsGeoCoder.getFeatureList().get(0).getName()); //での検索結果
+        model.addAttribute("coordinates", contentsGeoCoder.getFeatureList().get(0).getGeometry().getCoordinates()); //中心地点
+
+        //coordinatesをローカルサーチAPIに渡してレスポンスをバインド
+        //リクエスト先: ローカルサーチAPI
+        //coordinatesを緯度と経度に分割 latLng[0]:経度 latLng[1]:緯度
+        String[] latLng = contentsGeoCoder.getFeatureList().get(0).getGeometry().getCoordinates().split(",",0);
+        //中心(latLng)からの距離(km)を指定
+        String dist = "1";
+        //リクエスト用URL
+        final String REQUEST_URL_2 = "https://map.yahooapis.jp/search/local/V1/localSearch?appid=dj00aiZpPVd0eW04MVNNRGtUbSZzPWNvbnN1bWVyc2VjcmV0Jng9YTc-&output=json&results=10&gc=0115001&lat=" + latLng[1] + "&lng=" + latLng[0] + "&dist=" + dist;
+        //GET
+        String response = restTemplate.getForObject(REQUEST_URL_2, String.class);
+        //TODO: レスポンス用のクラスを作成してバインド
+
+        //Modelにadd
+        model.addAttribute("cafes", response);
 
         return "index";
     }
