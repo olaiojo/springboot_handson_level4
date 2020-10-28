3b106087ba06609c6d2af568cf3791df5c21b0a5
olaiojo <akhr.daigo@gmail.com>
2020-10-28 12:32:35 +0900

    add codes to confirm binding feature

--- src/main/java/com/daigo/springboot_handson_4/controller/MainController.java
+++ src/main/java/com/daigo/springboot_handson_4/controller/MainController.java
@@ -5,7 +5,11 @@ import com.daigo.springboot_handson_4.domains.ContentsGeoCoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Controller
 public class MainController {
@@ -15,16 +19,30 @@ public class MainController {
     public String index(Model model) {
         //開発用に湘南台駅の文字列を指定。
         final SampleLocation userLocation = new SampleLocation("湘南台駅");
+        model.addAttribute("userLocation", userLocation.getSAMPLE_LOCATION());
 
         //TODO: userLocationをコンテンツジオコーダAPIに渡してレスポンスをバインド
-        String REQUEST_URL = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder?appid=dj00aiZpPVd0eW04MVNNRGtUbSZzPWNvbnN1bWVyc2VjcmV0Jng9YTc-&query=湘南台駅&category=address&output=json";
-        final ContentsGeoCoder contentsGeoCoder = restTemplate.getForObject(REQUEST_URL, ContentsGeoCoder.class);
+        //リクエスト先URL: コンテンツジオコーダAPI
+        final String REQUEST_URL = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder?appid=dj00aiZpPVd0eW04MVNNRGtUbSZzPWNvbnN1bWVyc2VjcmV0Jng9YTc-&query=湘南台駅&category=address&output=json";
+        //ContentsGeoCoderクラスへのバインドをtry
+        ContentsGeoCoder contentsGeoCoder = new ContentsGeoCoder();
+        try {
+            contentsGeoCoder = restTemplate.getForObject(REQUEST_URL, ContentsGeoCoder.class);
+            model.addAttribute("geo", contentsGeoCoder.getResultInfo());
+        }catch (HttpClientErrorException e) {
+            System.out.println("|||||||||| Error 4XX ||||||||||");
+            throw e;
+        }catch (HttpServerErrorException e){
+            System.out.println("|||||||||| Error 5XX ||||||||||");
+            throw e;
+        }
 
         System.out.println("==========debug zone===========");
         System.out.println(REQUEST_URL);
+        System.out.println("contentsGeoCoder nonNull? : " + Objects.nonNull(contentsGeoCoder)); //true
+        System.out.println("ResultInfo nonNull? : " + Objects.nonNull(contentsGeoCoder.getResultInfo())); // false
+        System.out.println("Feature nonNull? : " + Objects.nonNull(contentsGeoCoder.getFeature())); // false
         System.out.println("==========/debug zone===========");
-
-        model.addAttribute("userLocation", userLocation.getSAMPLE_LOCATION());
 
         return "index";
     }
