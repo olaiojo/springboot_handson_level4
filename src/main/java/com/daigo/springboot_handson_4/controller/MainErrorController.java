d420552dad166d02d9b35a2edf74f8ff5b4733b1
olaiojo <akhr.daigo@gmail.com>
2021-01-17 19:38:24 +0900

    feat: Error message (friendly) to user.

--- src/main/java/com/daigo/springboot_handson_4/controller/MainErrorController.java
+++ src/main/java/com/daigo/springboot_handson_4/controller/MainErrorController.java
@@ -1,18 +1,16 @@
 package com.daigo.springboot_handson_4.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * ErrorControllerインタフェースの実装クラス
  */
-
 @Controller
 @RequestMapping("/error")
 public class MainErrorController implements ErrorController {
@@ -22,19 +20,42 @@ public class MainErrorController implements ErrorController {
         return "/error";
     }
 
+    /**
+     * エラーのステータスコードによってユーザに表示するメッセージを返却するメソッド。
+     *
+     * @param statusCode ステータスコード
+     * @return ユーザに表示するメッセージ
+     */
+    public String errorMessenger(Object statusCode) {
+        final int code = Integer.parseInt(String.valueOf(statusCode));
+        String message;
+        switch (code) {
+            case 404:
+                message = "お探しのページがありませんでした。";
+                break;
+            case 500:
+                message = "サーバ側でエラーが発生しました。";
+                break;
+            default:
+                message = "不明なエラーです。";
+                break;
+        }
+        return message;
+    }
+
     @RequestMapping
     public ModelAndView error(HttpServletRequest request) {
         // ERROR_STATUS_CODEを取得
         Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE); // xxx
         // ステータスコードをHttpStatus.valueOf()でHttpStatusに変換
         HttpStatus status = HttpStatus.valueOf((Integer) statusCode); //xxx_ERROR_ERROR
-
         // 出力情報のセット
         ModelAndView mav = new ModelAndView();
         mav.setStatus(status);
         mav.setViewName("error");
         mav.addObject("title", "Error!");
         mav.addObject("status", status);
+        mav.addObject("message", errorMessenger(statusCode));
         return mav;
     }
 }
