ff24a590ce2c4678d1e03ada96815a03c3a92f75
olaiojo <akhr.daigo@gmail.com>
2021-01-27 12:13:47 +0900

    refactor: do not map unnecessary fields.

--- src/main/java/com/daigo/springboot_handson_4/domains/localsearch/Property.java
+++ src/main/java/com/daigo/springboot_handson_4/domains/localsearch/Property.java
@@ -1,7 +1,7 @@
 package com.daigo.springboot_handson_4.domains.localsearch;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,38 +9,8 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Property {
-    @JsonProperty("Uid")
-    private String uid;
-    @JsonProperty("CassetteId")
-    private String cassetteId;
-    @JsonProperty("Yomi")
-    private String yomi;
-    @JsonProperty("Country")
-    private Country country;
     @JsonProperty("Address")
     private String address;
-    @JsonProperty("GovernmentCode")
-    private String governmentCode;
-    @JsonProperty("AddressMatchingLevel")
-    private String addressMatchingLevel;
-    @JsonProperty("Tel1")
-    private String tel1;
-    @JsonProperty("Genre")
-    private List<Genre> genreList;
-    @JsonProperty("Station")
-    private List<Station> stationList;
-    @JsonProperty("LeadImage")
-    private String leadImage;
-    //フラグ類は実質booleanだがダブルクオートで囲まれている
-    @JsonProperty("ParkingFlag")
-    private String parkingFlag;
-    @JsonProperty("CouponFlag")
-    private String couponFlag;
-    @JsonProperty("SmartPhoneCouponFlag")
-    private String smartPhoneCouponFlag;
-    @JsonProperty("Coupon")
-    private List<Coupon> couponList;
-    @JsonProperty("KeepCount")
-    private String keepCount;
 }
